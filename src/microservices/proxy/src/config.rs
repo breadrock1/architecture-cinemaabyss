3b5342a2ec1085c229567070825a10bbe47aba8d
breadrock1 <breadrock1@gmail.com>
2025-05-21 15:54:31 +0300

    fix: parsing env after config files

--- src/microservices/proxy/src/config.rs
+++ src/microservices/proxy/src/config.rs
@@ -4,10 +4,10 @@ use getset::{CopyGetters, Getters};
 use serde::Deserialize;
 
 const DEV_FILE_CONFIG_PATH: &str = "./config/development.toml";
-const SERVICE_RUN_MODE: &str = "RUN_MODE";
-const SERVICE_PREFIX: &str = "";
+const SERVICE_RUN_MODE: &str = "PROXY_SERVICE__RUN_MODE";
+const SERVICE_PREFIX: &str = "PROXY_SERVICE";
 
-#[derive(Clone, Deserialize, Getters, CopyGetters)]
+#[derive(Clone, Debug, Deserialize, Getters, CopyGetters)]
 #[getset(get = "pub")]
 pub struct ServiceConfig {
     logger: String,
@@ -38,7 +38,9 @@ impl ServiceConfig {
             .format(FileFormat::Toml)
             .required(false);
 
-        let env_config = Environment::with_prefix(SERVICE_PREFIX);
+        let env_config = Environment::with_prefix(SERVICE_PREFIX)
+            .separator("__")
+            .try_parsing(true);
 
         let settings = Config::builder()
             .add_source(dev_file_config)
